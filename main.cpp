52bd44ff952e2c43e426b5b1be99155e51741577
beginner1986 <beginner1986@gmail.com>
2021-01-12 18:19:29 +0100

    Implicit namespaces added everyvhere

--- main.cpp
+++ main.cpp
@@ -3,13 +3,11 @@
 #include <cmath>
 #include <armadillo>
 
-using namespace std;
-
 int main()
 {
     //matherial properties
     double A = 2;       // elements' cross area
-    double E = 10e6;     // Young's modulus
+    double E = 10e6;    // Young's modulus
 
     // nodes coordinates definition (each node x and y) in one table as they are also dofs
     const size_t nodesCount = 4;
@@ -39,7 +37,7 @@ int main()
     };
 
     // external forces applied to the truss
-    vector<double> externalForces = { 
+    std::vector<double> externalForces = { 
         0, 0,       // no forces at node 0
         0, 0,       // no forces at node 1
         0, 0,       // no forces at node 2
@@ -49,17 +47,16 @@ int main()
     // global stiffness matrix declaration
     arma::Mat<double> globalStiffness(dofsCount, dofsCount);
 
-    // elements' stiffness matrices
     // template for elements' stiffness matrices
-    arma::Mat<double> localStiffness(4, 4, arma::fill::zeros);
-    localStiffness(0, 0) = 1;
-    localStiffness(0, 2) = -1;
-    localStiffness(2, 0) = -1;
-    localStiffness(2, 2) = 1;
+    arma::Mat<double> localStiffnessTemplate(4, 4, arma::fill::zeros);
+    localStiffnessTemplate(0, 0) = 1;
+    localStiffnessTemplate(0, 2) = -1;
+    localStiffnessTemplate(2, 0) = -1;
+    localStiffnessTemplate(2, 2) = 1;
 
     for(size_t i=0; i<elementsCount; i++)
     {
-        cout << "ELEMENT " << i << endl;
+        std::cout << "ELEMENT " << i << std::endl;
         // element's degrees of freenoom matrix
         uint dofs[4] = { topology[i][0], topology[i][1], topology[i][2], topology[i][3] };
 
@@ -68,29 +65,29 @@ int main()
         double y1 = coordinates[dofs[1]];
         double x2 = coordinates[dofs[2]];
         double y2 = coordinates[dofs[3]];
-        cout << "\t(" << x1 << ", " << y1 << ") - (" << x2 << ", " << y2 << ")" << endl;
+        std::cout << "\t(" << x1 << ", " << y1 << ") - (" << x2 << ", " << y2 << ")" << std::endl;
 
-        // claculate the length nad parameters for trigonometry
+        // claculate the length and parameters for trigonometry
         double dx = abs(x2 - x1);
         double dy = abs(y2 - y1);
         double length = sqrt(dx * dx + dy * dy);
-        cout << "\tdx = " << dx << "; dy = " << dy << "; l = " << length << endl;
+        std::cout << "\tdx = " << dx << "; dy = " << dy << "; l = " << length << std::endl;
 
         // cosinus and sinus of the angle between local and global CS
         double cos = dx / length;
         double sin = dy / length;
-        cout << "\tcos = " << cos << "; sin =" << sin << endl;
+        std::cout << "\tcos = " << cos << "; sin =" << sin << std::endl;
 
         // beam stiffness value (scalar)
         double k = (A * E) / length;
-        cout << "\tk = " << k << endl;
+        std::cout << "\tk = " << k << std::endl;
 
         // element's stfiffness matrix in local CS
-        arma::Mat<double> elementStiffnessLocal = localStiffness;
+        arma::Mat<double> elementStiffnessLocal = localStiffnessTemplate;
         elementStiffnessLocal *= k;
 
-        cout << "\t element stiffness in local CS:" << endl;
-        cout << elementStiffnessLocal << endl;
+        std::cout << "\t element stiffness in local CS:" << std::endl;
+        std::cout << elementStiffnessLocal << std::endl;
 
         // transformation matrix
         arma::Mat<double> transtofrmationMatrix = {
@@ -99,34 +96,28 @@ int main()
             { -cos * cos, - cos * sin, cos * cos, cos * sin },
             { -cos * sin, -sin * sin, cos * sin, sin * sin }
         };
-        cout << "\tTransformation matrix:" << endl;
-        cout << transtofrmationMatrix << endl;
+        std::cout << "\tTransformation matrix:" << std::endl;
+        std::cout << transtofrmationMatrix << std::endl;
 
         // calculate element's stiffnesss matrix in global CS
         arma::Mat<double> elementStiffnessGlobal = transtofrmationMatrix * k;
 
-        cout << "\tElement stiffness matrix in global CS:" << endl;
-        cout << elementStiffnessGlobal << endl;
+        std::cout << "\tElement stiffness matrix in global CS:" << std::endl;
+        std::cout << elementStiffnessGlobal << std::endl;
 
-        // assembly global stiffness matrix
+        // assembly global stiffness matrix [4x4]
         for(size_t i=0; i<4; i++)
-        {
             for(size_t j=0; j<4; j++)
-            {
                 globalStiffness(dofs[i], dofs[j]) += elementStiffnessGlobal(i, j);
-                //cout << "\tdofs: " << dofs[i] << ", " << dofs[j] << endl;
-                //cout << "\tvalue = " << elementStiffnessGlobal(i, j) << endl << endl;
-            }
-        }
     }
 
-    cout << "Global stiffness matrix: " << endl;
-    cout << globalStiffness << endl;
+    std::cout << "Global stiffness matrix: " << std::endl;
+    std::cout << globalStiffness << std::endl;
 
     // global forces vector
     arma::Col<double> globalForces(externalForces);
-    cout << "Global forces vector: " << endl;
-    cout << globalForces << endl;
+    std::cout << "Global forces vector: " << std::endl;
+    std::cout << globalForces << std::endl;
 
     // global stiffness matrix and load vector reduction according to given constrains
     arma::Mat<double> reducedStiffness = globalStiffness;
@@ -142,17 +133,17 @@ int main()
         }
     }
 
-    cout << "Reduced stiffness matrix:" << endl;
-    cout << reducedStiffness << endl;
+    std::cout << "Reduced stiffness matrix:" << std::endl;
+    std::cout << reducedStiffness << std::endl;
 
-    cout << "Reduced loads' vector:" << endl;
-    cout << reducedForces << endl; 
+    std::cout << "Reduced loads' vector:" << std::endl;
+    std::cout << reducedForces << std::endl; 
 
     // reduced displacements' vector
     arma::Col<double> reducedDisplacements(dofsCount);
     arma::solve(reducedDisplacements, reducedStiffness, reducedForces);
-    cout << "Reduced displacements' vector: " << endl;
-    cout << reducedDisplacements << endl;
+    std::cout << "Reduced displacements' vector: " << std::endl;
+    std::cout << reducedDisplacements << std::endl;
 
     // global displacements' vector
     arma::Col<double> dispalcements(dofsCount, arma::fill::zeros);
@@ -164,8 +155,8 @@ int main()
             dispalcements(i) = reducedDisplacements(count++);
     }
 
-    cout << "Global displacements vector: " << endl;
-    cout << dispalcements << endl;
+    std::cout << "Global displacements vector: " << std::endl;
+    std::cout << dispalcements << std::endl;
 
     return 0;
 }
