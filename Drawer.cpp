38a0aafcdcbabe18a74675bb844d69fc974d368e
beginner1986 <beginner1986@gmail.com>
2021-01-14 20:17:17 +0100

    Elements drawing implemented

--- Drawer.cpp
+++ Drawer.cpp
@@ -5,6 +5,7 @@ Drawer::Drawer(const Truss &truss, std::string fileName) : truss(truss), fileNam
     svg::Dimensions dimensions = calculateDimensions();
     svg::Document document(fileName, svg::Layout(dimensions, svg::Layout::BottomLeft));
 
+    // draw the nodes
     for (size_t node = 0; node < truss.dofsCount; node += 2)
     {
         double x = truss.coordinates.at(node);
@@ -14,6 +15,23 @@ Drawer::Drawer(const Truss &truss, std::string fileName) : truss(truss), fileNam
         y = y * scale + offset;
 
         document << svg::Circle(svg::Point(x, y), 5, svg::Fill(svg::Color::Black));
+    }
+
+    // draw the elements
+    for (size_t element = 0; element < truss.elementsCount; element++)
+    {
+        arma::Row<uint> dofs = truss.topology.row(element);
+        double x1 = truss.coordinates.at(dofs(0));
+        double y1 = truss.coordinates.at(dofs(1));
+        double x2 = truss.coordinates.at(dofs(2));
+        double y2 = truss.coordinates.at(dofs(3));
+
+        x1 = x1 * scale + offset;
+        y1 = y1 * scale + offset;
+        x2 = x2 * scale + offset;
+        y2 = y2 * scale + offset;
+
+        document << svg::Line(svg::Point(x1, y1), svg::Point(x2, y2), svg::Stroke(2, svg::Color::Blue));
     }
 
     document.save();
--- Drawer.h
+++ Drawer.h
@@ -2,6 +2,7 @@
 #define __DRAWER_H__
 
 #include <algorithm>
+#include <armadillo>
 
 #include "lib/simple_svg_1.0.0.hpp"
 #include "Truss.h"
@@ -12,7 +13,7 @@ private:
     const Truss& truss;
     std::string fileName;
 
-    const double offset = 10;
+    const double offset = 50;
     const uint scale = 4;
 
 public:
