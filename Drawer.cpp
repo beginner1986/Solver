5e76c430f6575223358af1ca826a0031cc9d9752
beginner1986 <beginner1986@gmail.com>
2021-01-23 12:18:57 +0100

    Horizontal and vertical constrains drawing

--- Drawer.cpp
+++ Drawer.cpp
@@ -6,14 +6,14 @@ Drawer::Drawer(const Truss &truss, std::string fileName) : truss(truss), fileNam
     document = svg::Document(fileName, svg::Layout(dimensions, svg::Layout::BottomLeft));
 }
 
-void Drawer::draw() 
+void Drawer::draw()
 {
     drawNodes();
     drawElements();
     drawExternalForces();
     drawConstrains();
 
-    document.save();    
+    document.save();
 }
 
 svg::Dimensions Drawer::calculateDimensions()
@@ -35,7 +35,7 @@ svg::Dimensions Drawer::calculateDimensions()
         }
     }
 
-    if(std::max(maxX, maxY) < 100)
+    if (std::max(maxX, maxY) < 100)
         scale *= 100;
 
     maxX *= scale;
@@ -44,7 +44,7 @@ svg::Dimensions Drawer::calculateDimensions()
     return svg::Dimensions(maxX + 2 * offset, maxY + 2 * offset);
 }
 
-void Drawer::drawNodes() 
+void Drawer::drawNodes()
 {
     for (size_t node = 0; node < truss.dofsCount; node += 2)
     {
@@ -58,9 +58,8 @@ void Drawer::drawNodes()
     }
 }
 
-void Drawer::drawElements() 
+void Drawer::drawElements()
 {
-    // draw the elements
     for (size_t element = 0; element < truss.elementsCount; element++)
     {
         arma::Row<uint> dofs = truss.topology.row(element);
@@ -78,21 +77,20 @@ void Drawer::drawElements()
     }
 }
 
-void Drawer::drawExternalForces() 
+void Drawer::drawExternalForces()
 {
-    // draw external forces
     for (size_t dof = 0; dof < truss.dofsCount; dof++)
     {
         // TODO: draw the force on the outer side of the truss
 
         double force = truss.externalForces.at(dof);
 
-        if(force != 0)
+        if (force != 0)
         {
             // force line coordinates - not scaled
             double x2;
             double y2;
-            if(dof % 2 == 0)
+            if (dof % 2 == 0)
             {
                 x2 = truss.coordinates.at(dof);
                 y2 = truss.coordinates.at(dof + 1);
@@ -108,7 +106,7 @@ void Drawer::drawExternalForces()
             double x1 = x2;
             double y1 = y2;
 
-            if(dof % 2 == 0)
+            if (dof % 2 == 0)
                 x1 += (force > 0) ? -forceLineLnegth : forceLineLnegth;
             else
                 y1 += (force > 0) ? -forceLineLnegth : forceLineLnegth;
@@ -118,28 +116,28 @@ void Drawer::drawExternalForces()
             // arrowhead
             double arrowX1, arrowY1, arrowX2, arrowY2;
 
-            if(dof % 2 == 0)    // horizontal
+            if (dof % 2 == 0) // horizontal
             {
-                arrowY1 = y2 + arrowThickness / 2;  
+                arrowY1 = y2 + arrowThickness / 2;
                 arrowY2 = y2 - arrowThickness / 2;
 
-                if(force > 0)   // right
+                if (force > 0) // right
                     arrowX1 = x2 - arrowLength;
-                else    //left
+                else //left
                     arrowX1 = x2 + arrowLength;
-                
+
                 arrowX2 = arrowX1;
             }
-            else    // vertical
+            else // vertical
             {
                 arrowX1 = x2 + arrowThickness / 2;
                 arrowX2 = x2 - arrowThickness / 2;
 
-                if(force > 0)   // up
+                if (force > 0) // up
                     arrowY1 = y2 - arrowLength;
-                else    // down
+                else // down
                     arrowY1 = y2 + arrowLength;
-                
+
                 arrowY2 = arrowY1;
             }
 
@@ -150,59 +148,88 @@ void Drawer::drawExternalForces()
     }
 }
 
-void Drawer::drawConstrains() 
+void Drawer::drawConstrains()
 {
-    // draw constrains
-    for(size_t dof=0; dof<truss.dofsCount; dof+=2)
+    for (size_t dof = 0; dof < truss.dofsCount; dof ++)
     {
-        if(truss.constrains.at(dof))
+        if (truss.constrains.at(dof))
         {
             /*
             TODO:
-            - not duplicate constrains on one node (x and y) - one-axis and two-axis constrains
-            - horizontal and vertical fix orientation
             - constrains drawing on the outer side of the truss
             */
             double x, y;
 
-            if(dof % 2 == 0)    // x axis
+            if (dof % 2 == 0) // x axis
             {
                 x = truss.coordinates.at(dof);
                 y = truss.coordinates.at(dof + 1);
+                drawHorizontalConstrain(x, y);
             }
-            else    // y axis
+            else // y axis
             {
                 x = truss.coordinates.at(dof - 1);
                 y = truss.coordinates.at(dof);
+                drawVerticalConstrain(x, y);
             }
-            x = x * scale + offset;
-            y = y * scale + offset;
-            
-            // triangle with underline
-            svg::Polyline fix(svg::Stroke(2, svg::Color::Green));
-            fix << svg::Point(x - fixSize / 2, y - fixSize / 2) 
-                << svg::Point(x, y)
-                << svg::Point(x + fixSize / 2, y - fixSize / 2)
-                << svg::Point(x + fixSize * 3/4, y - fixSize / 2)
-                << svg::Point(x - fixSize * 3/4, y - fixSize / 2);
-            document << fix;
-
-            // diagonal lines
-            document << svg::Line(
-                svg::Point(x - fixSize * 3/5, y - fixSize), 
-                svg::Point(x - fixSize * 2/5, y - fixSize / 2), 
-                svg::Stroke(2, svg::Color::Green)
-            );
-            document << svg::Line(
-                svg::Point(x - fixSize / 5, y - fixSize), 
-                svg::Point(x, y - fixSize / 2), 
-                svg::Stroke(2, svg::Color::Green)
-            );
-            document << svg::Line(
-                svg::Point(x + fixSize * 1/5, y - fixSize), 
-                svg::Point(x + fixSize * 2/5, y - fixSize / 2), 
-                svg::Stroke(2, svg::Color::Green)
-            );
         }
     }
+}
+
+void Drawer::drawHorizontalConstrain(double x, double y)
+{
+    x = x * scale + offset;
+    y = y * scale + offset;
+
+    // triangle with underline
+    svg::Polyline fix(svg::Stroke(2, svg::Color::Green));
+    fix << svg::Point(x + fixSize / 2, y - fixSize / 2)
+        << svg::Point(x, y)
+        << svg::Point(x + fixSize / 2, y + fixSize / 2)
+        << svg::Point(x + fixSize / 2, y + fixSize * 3 / 4)
+        << svg::Point(x + fixSize / 2, y - fixSize * 3 / 4);
+    document << fix;
+
+    // diagonal lines
+    document << svg::Line(
+        svg::Point(x + fixSize / 2, y + fixSize * 2 / 5),
+        svg::Point(x + fixSize, y + fixSize / 5),
+        svg::Stroke(2, svg::Color::Green));
+    document << svg::Line(
+        svg::Point(x + fixSize / 2, y),
+        svg::Point(x + fixSize, y - fixSize / 5),
+        svg::Stroke(2, svg::Color::Green));
+    document << svg::Line(
+        svg::Point(x + fixSize / 2, y - fixSize * 2 / 5),
+        svg::Point(x + fixSize , y - fixSize * 3 / 5),
+        svg::Stroke(2, svg::Color::Green));
+}
+
+void Drawer::drawVerticalConstrain(double x, double y)
+{
+    x = x * scale + offset;
+    y = y * scale + offset;
+
+    // triangle with underline
+    svg::Polyline fix(svg::Stroke(2, svg::Color::Green));
+    fix << svg::Point(x - fixSize / 2, y - fixSize / 2)
+        << svg::Point(x, y)
+        << svg::Point(x + fixSize / 2, y - fixSize / 2)
+        << svg::Point(x + fixSize * 3 / 4, y - fixSize / 2)
+        << svg::Point(x - fixSize * 3 / 4, y - fixSize / 2);
+    document << fix;
+
+    // diagonal lines
+    document << svg::Line(
+        svg::Point(x - fixSize * 3 / 5, y - fixSize),
+        svg::Point(x - fixSize * 2 / 5, y - fixSize / 2),
+        svg::Stroke(2, svg::Color::Green));
+    document << svg::Line(
+        svg::Point(x - fixSize / 5, y - fixSize),
+        svg::Point(x, y - fixSize / 2),
+        svg::Stroke(2, svg::Color::Green));
+    document << svg::Line(
+        svg::Point(x + fixSize * 1 / 5, y - fixSize),
+        svg::Point(x + fixSize * 2 / 5, y - fixSize / 2),
+        svg::Stroke(2, svg::Color::Green));
 }
